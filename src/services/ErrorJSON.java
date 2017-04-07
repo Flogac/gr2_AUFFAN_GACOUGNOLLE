db15dfa469a17d37c47a888ea8120f5fb33c1cca
oraora20 <olivierora@gmail.com>
2017-04-07 11:33:39 +0200

    Add files via upload

--- src/services/ErrorJSON.java
+++ src/services/ErrorJSON.java
@@ -1,12 +1,22 @@
+//Codes d'erreur
+//0:succes
+
 package services;
 
 import org.json.JSONObject;
 
 public class ErrorJSON {
 
-	public static JSONObject serviceRefused(String string, int i) {
+	public static JSONObject serviceRefused(String message, int codeErreur){
+		System.out.println("entree dans serviceRefused de services.ErrorJSON");
 		JSONObject res=new JSONObject();
-		res.put(string,i);
+		res.put(message,codeErreur);
+		return res;
+	}
+	public static JSONObject serviceAccepted(){
+		System.out.println("entree dans serviceAccepted de services.ErrorJSON");
+		JSONObject res=new JSONObject();
+		res.put("succes",0);
 		return res;
 	}
 }
