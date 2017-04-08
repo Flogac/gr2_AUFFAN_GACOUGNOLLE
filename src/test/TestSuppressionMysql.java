15b294134ab8c07a5fa45c3d02444809c2daccfd
oraora20 <olivierora@gmail.com>
2017-04-08 11:41:43 +0200

    Add files via upload

---
+++ src/test/TestSuppressionMysql.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import bd.DatabaseServices;
+
+public class TestSuppressionMysql{
+	public static void main(String[] args){
+		String req="Friends";
+		List<String> colonnes=new ArrayList<String>();
+		List<String> valeurs=new ArrayList<String>();
+		colonnes.add("de");
+		colonnes.add("vers");
+		colonnes.add("timestamp");
+		valeurs.add("1234");
+		valeurs.add("5678");
+		valeurs.add(new Timestamp(System.currentTimeMillis()).toString());
+		try{
+			DatabaseServices.delete(req,colonnes,valeurs);
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+}
