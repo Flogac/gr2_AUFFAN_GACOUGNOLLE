03121d3f4fac28157f384a4a2cac6cc7de5d32c4
oraora20 <olivierora@gmail.com>
2017-04-12 10:32:38 +0200

    Add files via upload
    version enfin fonctionnelle!

--- src/test/TestInsertionMysqlEtUserExists.java
+++ src/test/TestInsertionMysqlEtUserExists.java
@@ -1,16 +1,27 @@
 package test;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 import bd.DatabaseServices;
+import bd.SessionsTools;
 
+//FONCTIONNE!!!!!
 public class TestInsertionMysqlEtUserExists{
-	public static void main(String[] args){
-		String req="Friends";
+	public static void main(String[] args) throws SQLException{
+		System.out.println("entree de main dans test.TestInsertionMysqlEtUserExists.java");
 		List<String> colonnes=new ArrayList<String>();
 		List<String> valeurs=new ArrayList<String>();
+		colonnes.add("login");
+		valeurs.add("1234");
+		DatabaseServices.insert("Users",colonnes,valeurs);
+		valeurs=new ArrayList<String>();
+		valeurs.add("5678");
+		DatabaseServices.insert("Users",colonnes,valeurs);
+		colonnes=new ArrayList<String>();
+		valeurs=new ArrayList<String>();
 		colonnes.add("de");
 		colonnes.add("vers");
 		colonnes.add("timestamp");
@@ -19,7 +30,8 @@ public class TestInsertionMysqlEtUserExists{
 		//permet d'ajouter un timestamp compatible avec mysql
 		valeurs.add(new Timestamp(System.currentTimeMillis()).toString());
 		//DatabaseServices.insert("",new ArrayList<String>(),new ArrayList<String>());
-		DatabaseServices.insert(req,colonnes,valeurs);
-		System.out.println("tout va bien");
+		DatabaseServices.insert("Friends",colonnes,valeurs);
+		System.out.println(SessionsTools.userExists("1234"));
+		System.out.println("sortie de main dans test.TestInsertionMysqlEtUserExists.java");
 	}
 }
