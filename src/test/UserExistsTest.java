ece0d180dc70db40eca3c8112e9cf20cd13f01f1
oraora20 <olivierora@gmail.com>
2017-05-03 17:47:23 +0200

    Add files via upload

---
+++ src/test/UserExistsTest.java
@@ -0,0 +1,33 @@
+//ce fichier teste bd.UsersTools.userExists et bd.DatabaseServices.exists: TERMINE
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import bd.DatabaseServices;
+import bd.UsersTools;
+
+public class UserExistsTest{
+	@Test
+	public void test(){
+		List<String> colonnes=new ArrayList<String>();
+		List<String> valeurs=new ArrayList<String>();
+		colonnes.add("login");
+		valeurs.add("1234");
+		try{
+			DatabaseServices.insert("Users",colonnes,valeurs);
+		}
+		catch(Exception e){}
+		try{
+			boolean res=UsersTools.userExists("1234");
+			assertTrue(true);
+			assertEquals(res,true);
+		}
+		catch(Exception e){}
+	}
+}
