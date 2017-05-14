7f613c50391ebbc37d562c1390d6890f9da1d441
OlivierAuffan <olivierora@gmail.com>
2017-05-14 11:25:37 +0200

    Add files via upload

---
+++ src/test/LoginTest.java
@@ -0,0 +1,30 @@
+//ce fichier teste services.UserServices.login: NON TERMINE
+//https://docs.oracle.com/javase/tutorial/jdbc/basics/retrieving.html
+package test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import services.UserServices;
+import bd.SessionsTools;
+
+
+public class LoginTest{
+	@Test
+	public void test(){
+		try{
+			UserServices.logout("test2",SessionsTools.getKey("test2"));
+		} 
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		try{
+			UserServices.login("test2","test");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
